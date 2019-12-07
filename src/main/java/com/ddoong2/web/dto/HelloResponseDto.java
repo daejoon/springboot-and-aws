38677237d2785aad43134d0d2e5aaf5ffaed04d1
daejoon <kkode1911@gmail.com>
2019-12-07 15:01:21 +0900

    02. 스프링 부트에서 테스트 코드를 작성하자

---
+++ src/main/java/com/ddoong2/web/dto/HelloResponseDto.java
@@ -0,0 +1,19 @@
+package com.ddoong2.web.dto;
+
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+
+    public HelloResponseDto(String name, int amount) {
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAmount() {
+        return this.amount;
+    }
+}
